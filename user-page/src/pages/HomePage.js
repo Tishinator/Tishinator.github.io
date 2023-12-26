901b22cab60bb16a59db845d71b3ed2b3136b733
Tishinator <mike.tishman@gmail.com>
2023-12-26 18:51:56 -0500

    Homepage initial commit

---
+++ user-page/src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './css/HomePage.css';
+
+function HomePage() {
+  return (
+    <div className="homepage">
+      <h1>Welcome to My Homepage</h1>
+      {/* Additional content will go here */}
+    </div>
+  );
+}
+
+export default HomePage;
