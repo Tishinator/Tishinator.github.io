a849bdac78e39676661f34cea57785d59cad6c1b
Tishinator <mike.tishman@gmail.com>
2024-01-01 22:32:14 -0500

    added git repo to the button

--- user-page/src/components/Projects.js
+++ user-page/src/components/Projects.js
@@ -10,7 +10,7 @@ const Projects = () => {
                 <p>The DatasetGenerator.py script is a Python program designed to apply transformations on an image.<br></br>These transformations will create a dataset from the single image to be used in machine learning.</p>
             </div>
             <Button variant='danger' style={{marginRight:"10px"}}>Live Demo</Button>
-            <Button variant='primary'>Git Repo</Button>
+            <a href='https://github.com/Tishinator/Dataset-Generator' ><Button variant='primary'>Git Repo</Button></a>
         </div>        
     );
 
