f32a1eb9a2691fa5baa9e70f7993cbddbbbfb6c5
Tishinator <mike.tishman@gmail.com>
2024-01-08 18:52:17 -0500

    updated datasetgenerator live demo link

--- user-page/src/components/Projects.js
+++ user-page/src/components/Projects.js
@@ -10,7 +10,7 @@ const Projects = () => {
                 <p>The DatasetGenerator.py script is a Python program designed to apply transformations on an image. These transformations will create a dataset from the single image to be used in machine learning. Original project was done for a machine learning project in college, but was branched out for uses on other projects.</p>
                 <p style={{textAlign:"center"}}> Live demo is a web-based application with the original dataset generator code translated to web serivces.</p>
             </div>
-            <Button variant='danger' style={{marginRight:"10px"}}>Live Demo</Button>
+            <a href='https://tishinator.github.io/dataset-generator-ui' ><Button variant='danger' style={{marginRight:"10px"}}>Live Demo</Button></a>
             <a href='https://github.com/Tishinator/Dataset-Generator' ><Button variant='primary'>Git Repo</Button></a>
         </div>        
     );
